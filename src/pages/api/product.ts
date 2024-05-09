d2a062d3ed9cdeabbded6de301b0c78ff1f6d3a7
IMHYEWON <hyewon1118@gmail.com>
2024-05-10 00:29:21 +0900

    SQL 작성해보기

--- src/pages/api/product.ts
+++ src/pages/api/product.ts
@@ -1,39 +1,40 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { QueryResult, sql } from "@vercel/postgres";
+
+type Product = {
+    product_id: number;
+    product_name: string;
+    price: number;
+    description: string;
+  };
 
 type Data = {
     name: string;
-    products: {
-      id: number;
-      name: string;
-      price: number;
-    }[];
+    products: Product[];
   };
   
-  export default function handler(
+  async function getProducts() : Promise<Product[]> {
+    'use verceldb';
+    const result: QueryResult = await sql`SELECT * FROM PRODUCT`;
+    const products: Product[] = result.rows.map((row) => {
+      return {
+        product_id: row.product_id,
+        product_name: row.product_name,
+        price: row.price,
+        description: row.description,
+      };
+    });
+
+    return products;
+  }
+
+  export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<Data>,
   ) {
-
-    res.status(200).json({
-        name: "John Doe",
-        products: [
-          {
-            id: 1,
-            name: "Product 1",
-            price: 100,
-          },
-          {
-            id: 2,
-            name: "Product 2",
-            price: 200,
-          },
-          {
-            id: 3,
-            name: "Product 3",
-            price: 300,
-          },
-        ],
-    })
+    const products = await getProducts();
+    const data: Data = { name: "John Doe", products : products }; // products의 형식을 정의
+    res.status(200).json(data); 
 
 }
   

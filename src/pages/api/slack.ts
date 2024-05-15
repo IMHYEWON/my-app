1f0d432683ae588977e067b5afe603f3c7afb982
IMHYEWON <hyewon1118@gmail.com>
2024-05-16 00:38:48 +0900

    후아유?

---
+++ src/pages/api/slack.ts
@@ -0,0 +1,39 @@
+// pages/api/send-to-slack.ts
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+const webhookUrl = process.env.SLACK_WEBHOOK_URL;
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'POST') {
+    const { message } = req.body;
+
+    if (!message) {
+      return res.status(400).json({ error: 'Message is required' });
+    }
+
+    try {
+      const response = await fetch(webhookUrl!, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ text: message }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to send message to Slack');
+      }
+
+      res.status(200).json({ success: true });
+    } catch (error) {
+      if (error instanceof Error) {
+        res.status(500).json({ error: error.message });
+      } else {
+        res.status(500).json({ error: 'An unknown error occurred' });
+      }
+    }
+  } else {
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}

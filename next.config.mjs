fad13867d06b941f56a1a3bfee10061e1a2e175d
IMHYEWON <hyewon1118@gmail.com>
2024-05-17 01:49:35 +0900

    랜덤 아이콘 애니메이션

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    SLACK_WEBHOOK_URL: process.env.SLACK_WEBHOOK_URL,
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { Pixelify_Sans } from "next/font/google";
 import MessageInput from '@/components/whoareyou';
+import Header from "@/components/header";
 
 const pixelify = Pixelify_Sans({ subsets: ["latin"] });
 
@@ -9,9 +10,7 @@ export default function Home() {
     <main
       className={`py-[15vh] sm:py-[20vh] max-w-md mx-4 sm:mx-auto space-x-2 md:space-x-8 flex flex-col items-center justify-center ${pixelify.className}`}
         >
-      <h1 className="transition hover:scale-110 z-10 text-2xl sm:text-4xl animate-title font-display whitespace-nowrap bg-clip-text mb-8">
-        Welcome ! I am Hyewon
-      </h1>
+      <Header />
       <div className="w-full animate-in fade-in slide-in-from-bottom-4 duration-1200 ease-in-out">        <MessageInput />
       </div>
     </main>

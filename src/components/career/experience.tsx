77fd2df9e097cb28f38fadec85aa8e8487ba271f
IMHYEWON <hyewon1118@gmail.com>
2024-05-19 18:33:15 +0900

    반응형 수정

--- src/components/career/experience.tsx
+++ src/components/career/experience.tsx
@@ -26,21 +26,21 @@ export default function Experience() {
                         <span className="inline text-sm font-normal text-teal-500"> 재직중</span>
                         <h3 className="text-lg font-semibold text-gray-900 dark:text-white mb-2">롯데 e커머스(롯데ON)</h3>
                         <span className="inline text-base text-gray-500 dark:text-white">SO개발팀</span>
-                        <p className={`${interLight.className} mb-8 text-base font-normal text-gray-700 dark:text-gray-400`}>
+                        <p className={`${interLight.className} mb-8 text-sm sm:text-base font-normal text-gray-700 dark:text-gray-400`}>
                             MSA 기반의 이커머스 플랫폼을 개발하고 있습니다. Seller Office 및 Back Office 공통시스템을 개발하고 있습니다.
                             주로 Java, Spring, Mybatis, Kafka, Redis, MySQL, Gitlab, Grafana 등을 사용하고 있습니다.
                         </p>
                         <p className={`${inter.className} underline text-teal-500 decoration-teal-500 ml-4 mb-2 text-md`}>Projects</p>
-                        <ol className="relative border-s border-gray-200 dark:border-gray-700 ml-4">
+                        <ol className="relative border-s border-gray-200 dark:border-gray-700 ml-2 sm:ml-4">
                             {lotteProject && lotteProject.projects && lotteProject.projects.map((project:Project, index:number) => (
                             <li className="mb-6 ms-4" key={project.id}>
                                 <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
                                 <span className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">{project.period.start} ~ </span>
                                 <span className="inline text-sm font-normal text-gray-400 dark:text-gray-500">{project.period.end}</span>
-                                <h3 className="text-lg font-semibold text-gray-900 dark:text-white mb-2">{project.title}</h3>
+                                <h3 className="text-base sm:text-lg font-semibold text-gray-900 dark:text-white mb-2">{project.title}</h3>
                                 {
                                     project.description.map((desc:string, index:number) => (
-                                        <p key={index} className={`${interLight.className} mb-1 text-base font-normal text-gray-700 dark:text-gray-400`}>
+                                        <p key={index} className={`${interLight.className} mb-1 text-sm sm:text-base font-normal text-gray-700 dark:text-gray-400`}>
                                             {desc}
                                         </p>
                                     ))
@@ -54,20 +54,20 @@ export default function Experience() {
                         <time className="mb-1 text-sm font-normal leading-none text-gray-40 dark:text-gray-500">2020.10 ~ 2022.05</time>
                         <h3 className="text-lg font-semibold text-gray-900 dark:text-white mb-2">웹케시</h3>
                         <span className="inline text-base text-gray-500 dark:text-white">rERP개발팀</span>
-                        <p className={`${interLight.className} mb-8 text-base font-normal text-gray-700 dark:text-gray-400`}>
+                        <p className={`${interLight.className} mb-8 text-sm sm:text-base font-normal text-gray-700 dark:text-gray-400`}>
                             웹 ERP 서비스를 개발하고 유지운영했습니다. 주로 대학교 산학협력단에서 사용하는 ERP서비스를 담당하여 구매, 연구비 등의 업무 시스템을 개발했습니다.
                         </p>
                         <p className={`${inter.className} underline text-teal-500 decoration-teal-500 ml-4 mb-2 text-md`}>Projects</p>
-                        <ol className="relative border-s border-gray-200 dark:border-gray-700 ml-4">
+                        <ol className="relative border-s border-gray-200 dark:border-gray-700 ml-2 sm:ml-4">
                             {webcashProject && webcashProject.projects && webcashProject.projects.map((project:Project, index:number) => (
                             <li className="mb-6 ms-4" key={project.id}>
                                 <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
                                 <span className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">{project.period.start} ~ </span>
                                 <span className="inline text-sm font-normal text-gray-400 dark:text-gray-500">{project.period.end}</span>
-                                <h3 className="text-lg font-semibold text-gray-900 dark:text-white mb-2">{project.title}</h3>
+                                <h3 className="text-base sm:text-lg font-semibold text-gray-900 dark:text-white mb-2">{project.title}</h3>
                                 {
                                     project.description.map((desc:string, index:number) => (
-                                        <p key={index} className={`${interLight.className} mb-1 text-base font-normal text-gray-700 dark:text-gray-400`}>
+                                        <p key={index} className={`${interLight.className} mb-1 text-sm sm:text-base font-normal text-gray-700 dark:text-gray-400`}>
                                             {desc}
                                         </p>
                                     ))
--- src/pages/career/index.tsx
+++ src/pages/career/index.tsx
@@ -43,11 +43,11 @@ export default function Career() {
     const [isHobby, setIsHobby] = useState(false);
 
     const handleToggle = (value: boolean) => {
-      setIsHobby(value);
+        setIsHobby(value);
     }
-  
+
     const currentTechStack = isHobby ? hobbyTechStack : productionTechStack;
-  
+
 
     return (
         <>
@@ -73,16 +73,16 @@ export default function Career() {
                                 </Link>
                             </div>
                         </div>
-                        <div className={`${inter.className} tracking-wide text-base text-gray-800 mt-6 mx-10 sm:mx-40 md:mx-52`}>
+                        <div className={`${inter.className} tracking-wide text-sm sm:text-base text-gray-800 mt-6 mx-5 sm:mx-40 md:mx-52`}>
                             <span className="inline underline decoration-2 decoration-blue-600/[.33] font-bold">사람을 좋아하는 개발자</span>
                             <span className="font-medium">입니다. 조직의 구성원과 함께 커뮤니케이션하는 것이 즐겁습니다. 나와는 다른 의견에는 흥미로움을 느끼며 같은 의견에는 짜릿함을 느낍니다. </span>
                         </div>
-                        <div className={`${inter.className} tracking-wide text-base text-gray-800 mt-2 mx-10 sm:mx-40 md:mx-52`}>
+                        <div className={`${inter.className} tracking-wide text-sm sm:text-base text-gray-800 mt-2 mx-5 sm:mx-40 md:mx-52`}>
                             <span className="inline underline decoration-2 decoration-orange-600/[.33] font-bold">합리적</span>
                             <span className="font-medium">입니다. 조직이 가지고 있는 리소스, 마주친 문제, 현재 조직의 문화를 고려해 최적의 의사결정을 만드려고 노력합니다.
                                 조직의 구성원 다수가 불편함을 느끼지 않는 한에서 새로운 의견과 기술을 도입하려고 노력합니다. 간혹 어떤이는 불편해하기도 하지만, 제가 가진 실천력과 성과로서 마음을 열게하는데는 자신 있습니다. </span>
                         </div>
-                        <div className={`${inter.className} tracking-wide text-base text-gray-800 mt-2 mx-10 sm:mx-40 md:mx-52`}>
+                        <div className={`${inter.className} tracking-wide text-sm sm:text-base text-gray-800 mt-2 mx-5 sm:mx-40 md:mx-52`}>
                             <span className="inline underline decoration-2 decoration-teal-600/[.33] font-bold">소심한 관종</span>
                             <span className="font-medium">입니다. 조직이 당면한 문제나 불편함을 해결한 경우 Confluent 혹은 Slack으로 해결과정을 공유하는 것을 좋아합니다. (늘어가는 이모지 수와 좋아요 수를 보고 행복함을 느낍니다.)</span>
                         </div>
@@ -96,11 +96,10 @@ export default function Career() {
                                 <TabList className="">
                                     {currentTechStack.skills.map((skill, index) => (
                                         skill && skill.items.map((item, index) => (
-                                            <Tab key={index} className="text-sm mr-1 mb-1 px-4 py-2 bg-gray-100 rounded-full hover:bg-gray-200 transition">{item}</Tab>
+                                            <Tab key={index} className="text-sm mr-1 mb-1 px-3 py-1.5 sm:px-4 sm:py-2 bg-gray-100 rounded-full hover:bg-gray-200 transition">{item}</Tab>
                                         ))
                                     ))
                                     }
-
                                 </TabList>
                             </TabGroup>
                         </div>
@@ -116,19 +115,12 @@ export default function Career() {
                             <li className="mb-10 ms-4">
                                 <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
                                 <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">2015.03 ~ 2021.02</time>
-                                <h3 className="text-lg font-semibold text-gray-900 dark:text-white">세종대학교 소프트웨어학과 졸업</h3>
-                                {/* <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages.</p> */}
-                                {/* <a href="#" className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-100 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">Learn more
-                                    <svg className="w-3 h-3 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                                    </svg>
-                                </a> */}
+                                <h3 className="text-base sm:text-lg font-semibold text-gray-900 dark:text-white">세종대학교 소프트웨어학과 졸업</h3>
                             </li>
                             <li className="mb-10 ms-4">
                                 <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
                                 <time className="mb-1 text-sm font-normal leading-none text-gray-40 dark:text-gray-500">2011.03 ~ 2014.02</time>
-                                <h3 className="text-lg font-semibold text-gray-900 dark:text-white">구리 인창고등학교 졸업</h3>
-                                {/* <p className="text-base font-normal text-gray-500 dark:text-gray-400">All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.</p> */}
+                                <h3 className="text-base sm:text-lg font-semibold text-gray-900 dark:text-white">구리 인창고등학교 졸업</h3>
                             </li>
                         </ol>
                     </div>

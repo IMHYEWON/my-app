508905bc7fc13119f0b9a147d1f0fac8f3c6680b
IMHYEWON <hyewon1118@gmail.com>
2024-06-05 00:25:11 +0900

    UI : 경력란 수정

--- src/components/career/experience.tsx
+++ src/components/career/experience.tsx
@@ -30,7 +30,6 @@ export default function Experience({ projects }: ProjectProps) {
     const lotteProject = projects && projects.filter((project) => project.project_name === '롯데e커머스');
     const webcashProject = projects && projects.filter((project) => project.project_name === '웹케시');
 
-    console.log('lotteProject:', lotteProject);
     return (
         <>
             <div className={`${inter.className} tracking-wide self-start mt-14 sm:mx-20 sm:px-20`}>
@@ -40,9 +39,9 @@ export default function Experience({ projects }: ProjectProps) {
                         <div className="absolute w-3 h-3 bg-teal-400 rounded-full mt-1.5 -start-1.5 border border-teal-400 dark:border-teal-900 dark:bg-teal-300"></div>
                         <span className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-stone-400">2022.12 ~ </span>
                         <span className="inline text-sm font-normal text-teal-500"> 재직중</span>
-                        <div className="flex items-center">
-                            <h3 className="text-xl font-semibold text-gray-900 dark:text-white mb-2">롯데 e커머스(롯데ON)</h3>
-                            <h3 className="ml-4 text-base text-gray-600 dark:text-white mb-2">롯데쇼핑 통합 e커머스 플랫폼</h3>
+                        <div className="flex sm:flex-row content-center flex-col">
+                            <h3 className="text-xl font-semibold text-gray-900 dark:text-white mt-1 sm:mb-2 mr-4">롯데 e커머스(롯데ON)</h3>
+                            <h3 className="text-base text-gray-600 dark:text-white sm:self-center">롯데쇼핑 통합 e커머스 플랫폼</h3>
                         </div>
 
                         <span className="inline text-base text-gray-500 dark:text-white">SO개발팀, 정직원</span>
@@ -82,7 +81,7 @@ export default function Experience({ projects }: ProjectProps) {
                                             return (
                                                 <div key={desc.id} className="flex flex-row items-start">
                                                     <svg className="mr-2 flex-shrink-0 w-3 h-3 text-teal-500 dark:text-teal-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5" />
+                                                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5.917 5.724 10.5 15 1.5" />
                                                     </svg>
                                                     <p key={desc.id} className={className}>
                                                         {'' + desc.description}
@@ -105,17 +104,19 @@ export default function Experience({ projects }: ProjectProps) {
                     <li className="mb-10 ms-4">
                         <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-stone-900 dark:bg-stone-500"></div>
                         <time className="mb-1 text-sm font-normal leading-none text-gray-40 dark:text-stone-400">2020.10 ~ 2022.05</time>
-                        <div className="flex items-center">
-                            <h3 className="text-xl font-semibold text-gray-900 dark:text-white mb-2">웹케시</h3>
-                            <h3 className="ml-4 text-base text-gray-600 dark:text-white mb-2">연구관리, 연구성과, 지식재산권, 회계, 인사급여 등 산학협력단 업무관리 서비스</h3>
+                        <div className="flex sm:flex-row content-center flex-col">
+                            <h3 className="text-xl text-wrap font-semibold text-gray-900 dark:text-white  mt-1 sm:mb-2 mr-4">웹케시</h3>
+                            <h3 className="text-base text-gray-600 dark:text-white sm:self-center">연구관리, 지식재산권, 회계, 인사급여 등 산학협력단 업무관리 서비스</h3>
                         </div>
-                        <span className="inline text-base text-gray-500 dark:text-white">rERP개발팀</span>
-                        <p className={`${interLight.className} mb-3 text-sm sm:text-base font-normal text-gray-700 dark: dark:text-stone-100`}>
-                            <b>Saas, On-Premise 형태의 웹 ERP 서비스를 개발하고 유지 운영</b>했습니다. 주로 대학교 산학협력단을 위한 ERP 서비스를 담당했고, 결재, 과제, 연구원 계약 등의 서비스를 개발했습니다.
-                        </p>
-                        <p className={`${interLight.className} mb-8 text-sm sm:text-base font-normal text-gray-700 dark: dark:text-stone-100`}>
-                            외부 연구비 집행 관리 시스템과 연계할 수 있는 인터페이스, 배치를 개선 및 관리해 시스템의 안정성에 기여했습니다.
-                        </p>
+                        <span className="inline text-base text-gray-500 dark:text-white">rERP개발팀, 정직원</span>
+                        <div className="mt-2">
+                            <p className={`${interLight.className} mb-3 text-sm sm:text-base font-normal text-gray-700 dark: dark:text-stone-100`}>
+                                <b>Saas, On-Premise 형태의 웹 ERP 서비스를 개발하고 유지 운영</b>했습니다. 주로 대학교 산학협력단을 위한 ERP 서비스를 담당했고, 결재, 과제, 연구원 계약 등의 서비스를 개발했습니다.
+                            </p>
+                            <p className={`${interLight.className} mb-8 text-sm sm:text-base font-normal text-gray-700 dark: dark:text-stone-100`}>
+                                외부 연구비 집행 관리 시스템과 연계할 수 있는 인터페이스, 배치를 개선 및 관리해 시스템의 안정성에 기여했습니다.
+                            </p>
+                        </div>
                         <p className={`${inter.className} underline text-teal-500 decoration-teal-500 ml-4 mb-2 text-md`}>Projects</p>
                         <ol className="relative border-s border-gray-200 dark:border-stone-500 ml-2 sm:ml-4">
                             {webcashProject && webcashProject.map((project: Project, index: number) => (
@@ -136,7 +137,7 @@ export default function Experience({ projects }: ProjectProps) {
                                             return (
                                                 <div key={desc.id} className="flex flex-row items-start">
                                                     <svg className="mr-2 flex-shrink-0 w-3 h-3 text-teal-500 dark:text-teal-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5" />
+                                                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5.917 5.724 10.5 15 1.5" />
                                                     </svg>
                                                     <p key={desc.id} className={className}>
                                                         {'' + desc.description}
--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -22,7 +22,7 @@ const ProductsPage = ({ products }: ProductsProps) => (
             {products.map((product) => (
                 <li className="flex items-center space-x-3 rtl:space-x-reverse" key={product.product_id}>
                     <svg className="flex-shrink-0 w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5"/>
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5.917 5.724 10.5 15 1.5"/>
                     </svg>
                 <h2 className="font-semibold">{product.product_name}</h2>
                 <p>Price: {product.price}</p>
